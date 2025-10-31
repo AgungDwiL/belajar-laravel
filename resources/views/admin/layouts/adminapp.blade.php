8d50b22205379a2e673a4b2c975ce34d05d37bbb
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-11-01 00:35:20 +0700

    menambahkan dashboard admin

---
+++ resources/views/admin/layouts/adminapp.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+        <!-- Custom styles for this template -->
+        <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+
+        {{-- Fill title page here --}}
+        <title>@yield('page-title')</title>
+
+    </head>
+
+    <body>
+        {{-- Navbar --}}
+        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="/admin/dashboard">PT Eterna Karya Sejahtera</a>
+            <div class="dropdown">
+               <button class="btn btn-dark dropdown-toggle d-flex align-items-center mr-4" type="button" data-toggle="dropdown" aria-expanded="false">
+                    <!-- Icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person-circle mr-2" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                    </svg>
+                    <!-- Username -->
+                    <span class="ml-1">Username</span>
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Sign Out</a>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container-fluid">
+            <div class="row">
+                {{-- Sidebar --}}
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
+                    <ul class="nav flex-column mt-3">
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('admin/dashboard*') ? 'active' : '' }}" href="/admin/dashboard">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('admin/products*') ? 'active' : '' }}" href="/admin/products">Products Index</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('admin/users*') ? 'active' : '' }}" href="/admin/users">Users Index</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+                    @yield('content')
+                </main>
+            </div>
+        </div>
+
+        {{-- Script JS CDN --}}
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('guest.home');
-});
-
+// Guest page
+Route::view('/', ('guest.home'));
 Route::view('/login', 'guest.login');
 Route::view('/register', 'guest.register');
 
@@ -14,6 +12,8 @@ Route::get('/add-products', function () {
     return view('add-products');
 });
 
+// Admin page
+Route::view('/admin/dashboard','admin.dashboard');
 Route::get('/products', 'ProductController@index');
 Route::get('/add-products', [ProductController::class, 'create'])->name('products.create');
 Route::post('/add-products', [ProductController::class, 'store'])->name('products.store');
